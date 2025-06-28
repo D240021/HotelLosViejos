5e52a2f702292271f4c3245375261c6a9aa9305d
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-06-27 22:35:49 -0600

    Se agrega mensaje de validación para una habitacion deshabilitada

--- Backend/src/main/java/com/hotelLosViejos/HotelLosViejos/Infraestructura/Excepciones/ManejarExcepciones.java
+++ Backend/src/main/java/com/hotelLosViejos/HotelLosViejos/Infraestructura/Excepciones/ManejarExcepciones.java
@@ -1,6 +1,7 @@
 package com.hotelLosViejos.HotelLosViejos.Infraestructura.Excepciones;
 
 import com.hotelLosViejos.HotelLosViejos.Presentacion.DTOs.Excepciones.ExcepcionDTO;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,11 @@ public class ManejarExcepciones {
     @ExceptionHandler(ReservaExcepcion.class)
     public ResponseEntity<ExcepcionDTO> manejarReservaExcepcion(ReservaExcepcion e){
         return ResponseEntity.unprocessableEntity().body(new ExcepcionDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(HabitacionDeshabilitadaExcepcion.class)
+    public ResponseEntity<ExcepcionDTO> manejarHabitacionDeshabilitadaExcepcion(HabitacionDeshabilitadaExcepcion e){
+        return ResponseEntity.status(HttpStatusCode.valueOf(410)).body(new ExcepcionDTO(e.getMessage()));
     }
 
 }
